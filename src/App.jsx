39d52c482f67bfc0d72af1d6fc5138dbbf178ee0
Mateus Beltrão <mateusbeltrao2019@gmail.com>
2024-02-29 08:15:02 -0300

    add Residencias

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 import './App.css'
 import { Companies } from "./components/Companies/Companies";
+import Residencias from "./components/Residencias/Residencias";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
         <Hero></Hero>
       </div>
       <Companies></Companies>
+      <Residencias></Residencias>
     </div>
   );
 }
