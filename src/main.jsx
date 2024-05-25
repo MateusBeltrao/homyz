e2872cd8bce6efc905fadb6bf75802b9984af933
Mateus Beltrão <mateusbeltrao2019@gmail.com>
2024-05-25 07:27:57 -0300

    add link

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,19 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider 
-      domain="dev-q8cx154qb7b3j6vd.us.auth0.com"
-      clientId="hdrNTtHrlDmKK1OGO7WpBcp6QhB1JfDd"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}
-      audience="https://homyz-front-git-main-mateusbeltraos-projects.vercel.app"
-      scope="openid profile email"
-    >
-      <App />
-    </Auth0Provider>
+  domain="dev-q8cx154qb7b3j6vd.us.auth0.com"
+  clientId="hdrNTtHrlDmKK1OGO7WpBcp6QhB1JfDd"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+  audience="https://homyz-front-git-main-mateusbeltraos-projects.vercel.app"
+  scope="openid profile email"
+>
+  <App />
+</Auth0Provider>
+
   </React.StrictMode>
 );
+
+
+homyz
